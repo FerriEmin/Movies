43075f2b1093fc0598624dea842d08f0606e7670
Ferri_Emin <ferri_emin@hotmail.com>
2023-10-08 00:04:56 +0200

    SDK base implementation

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Refit;
+using Movies.Api.Sdk;
+using System.Text.Json;
+using Movies.Contracts.Requests;
+using Microsoft.Extensions.DependencyInjection;
+
+//var moviesApi = RestService.For<IMoviesApi>("https://localhost:7218");
+
+var services = new ServiceCollection();
+
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7218")); 
+
+var serviceProvider = services.BuildServiceProvider();
+
+var moviesApi = serviceProvider.GetRequiredService<IMoviesApi>();
+
+//var movie = await moviesApi.GetMovieAsync("ferris-day-uneployed-3--happy-worker-2023");
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 4
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
+
---
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,21 @@
+﻿using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Sdk
+{
+    public interface IMoviesApi
+    {
+        [Get(ApiEndpoints.Movies.Get)]
+        Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+        [Get(ApiEndpoints.Movies.GetAll)]
+        Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
+        Task GetServicesAsync();
+    }
+}
